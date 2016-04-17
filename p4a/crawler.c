fa686e57b826185e658e246d338ff23a951a8df1
lewiskunik <lkunik@wisc.edu>
2016-04-17 18:11:57 -0500

    stuck at pagea hash index

--- p4a/crawler.c
+++ p4a/crawler.c
@@ -8,14 +8,17 @@
 
 #include <time.h>
 
+#define HASH_TABLE_SIZE 193
+
 //Global definitions for the hash table
 typedef struct HashNode_t{
 	struct HashNode_t *next;
 	char *link;
+	//int done;
 }HashNode;
 
 //193 is a prime number! That's the size of our array.
-HashNode *HashTable[193];
+HashNode *HashTable[HASH_TABLE_SIZE];
 
 typedef struct Node_t{
 	struct Node_t *next;
@@ -41,6 +44,8 @@ int dfillptr = 0;
 int duseptr = 0;
 int dq_size;
 int downldItems = 0;
+
+int num_awake = 0;
 
 Queue *buff_parse;
 
@@ -83,7 +88,7 @@ uint32_t fletcher32( uint16_t const *data, size_t words )
 
 //TODO: may need to initialize hash table so each node is null
 /*void hash_init(){
-	HashTable = (*HashNode[])malloc(193 * sizeof(HashNode*));
+	HashTable = (*HashNode[])malloc(HASH_TABLE_SIZE * sizeof(HashNode*));
 }*/
 
 
@@ -91,7 +96,7 @@ uint32_t fletcher32( uint16_t const *data, size_t words )
 int check_hash(char *link){
 
 	
-	int index = fletcher32((uint16_t const *)link, strlen(link)) % 193;
+	int index = fletcher32((uint16_t const *)link, strlen(link)) % HASH_TABLE_SIZE;
 	printf("Link is: %s and Hash index is: %d\n", link, index);
 	int first_time = 1;
 	HashNode *check_node, *prev_node;
@@ -110,6 +115,7 @@ int check_hash(char *link){
 	}
 	HashNode *new_node = malloc(sizeof(HashNode *));
 	new_node->next = NULL;
+	//new_node->done = 0;
 	new_node->link = link;
 	if(first_time)
 		HashTable[index] = new_node;
@@ -118,6 +124,28 @@ int check_hash(char *link){
 	
 	return 0;
 }
+
+//sets the hash node of the link in the table to the done state.
+/*
+void set_hash_done(char *link){
+
+	int index = fletcher32((uint16_t const *)link, strlen(link)) % HASH_TABLE_SIZE;
+	HashNode *check_node;
+	check_node = HashTable[index];
+	//should return the node because it has already been downloaded
+	while(check_node != NULL){		
+		if((strcmp(link, check_node->link)) == 0){
+			printf("found used link\n");
+			continue;
+		}
+		else
+			check_node = check_node->next;
+	}
+	if(check_node != NULL)
+		check_node->done = 1;
+	//else: do nothing. return from function. should never actually leave without setting done to 1
+}
+*/
 
 
 ///////////////////////
@@ -199,41 +227,62 @@ Node *pget(){
 // parsing function
 //////////////////
 void parse(char* prev_link, char* page){
-	char *tmp, *link;
+	char *tmp, *link, *copy;
 	char *saveptr = NULL;
+	copy = strdup(page);
 	tmp = strtok_r(page, ":", &saveptr);//problem child
-	printf("file is: %s and tmp = %s\n", prev_link, tmp);
+	printf("file is: %s\n", prev_link);
+	if(strcmp(tmp, copy) == 0)
+		return;
 	while(tmp != NULL){
 		
-		link = strdup(strtok_r(NULL, " \n", &saveptr));
+		//link = strdup(strtok_r(NULL, " \n", &saveptr));
+		link = strtok_r(NULL, " \n", &saveptr);
 		printf("link found: %s\n", link);
 		pthread_mutex_lock(&d_lock);
 		//wait on condition variable if download queue is full
-		printf("downldItems = %d\n", downldItems);
+		if(downldItems == dq_size)
+			num_awake--;
 		while(downldItems == dq_size)
 			pthread_cond_wait(&dfull, &d_lock);
+		num_awake++;
 		if(!check_hash(link)){
 			dfill(link);//means that the link has not been downloaded before
 			pthread_cond_signal(&dempty);
 		}
+
 		pthread_mutex_unlock(&d_lock);
 		//send link via edge. use link and prev_link
 		_edge_function(prev_link, link);
+
+		copy = strdup(tmp);
 		tmp = strtok_r(NULL, ":", &saveptr);
+
+		if(tmp != NULL){//
+		/// this compares strings to see if we've reached the end of the file
+		if(strcmp(tmp, copy) == 0){
+			tmp = NULL;
+		}
+		}//
 	}
+	
 }
 
 
 
 //producer sequence
 void *parse_thread(){
+	
 	Node *page_content;
 	while(1){
-		printf("parser\n");
+		//printf("parser\n");
 		pthread_mutex_lock(&p_lock);
+		if(buff_parse->parseItems == 0)
+			num_awake--;
 		//if parse queue is empty, wait until dowloader gives something
 		while(buff_parse->parseItems == 0)
 			pthread_cond_wait(&pempty, &p_lock);
+		num_awake++;
 		page_content = pget();
 		pthread_mutex_unlock(&p_lock);
 		parse(page_content->parent_link, page_content->data); //will implement the pthread_wait within
@@ -244,24 +293,28 @@ void *parse_thread(){
 
 //consumer sequence
 void *download_thread(){
-
+	
 	while(1){
-		printf("downloader\n");
+		//printf("downloader\n");
 		//is download queue empty? If yes, pull from parse queue and get a link!
 		pthread_mutex_lock(&d_lock);
 		//if download queue is empty, wait until parser gives it something
-		printf("%d\n", downldItems);
+		//printf("%d\n", downldItems);
+		if(downldItems == 0)
+			num_awake--;
 		while(downldItems == 0)
 			pthread_cond_wait(&dempty, &d_lock);
-
+		num_awake++;
 		//pull link off of download queue
 		char *link = dget();
 		pthread_cond_signal(&dfull);
 		pthread_mutex_unlock(&d_lock);
+		
 
 		char *page = _fetch_function(link);
 		//add content to parse queue
 		padd(link, page);
+		//set_hash_done(link);
 		pthread_cond_signal(&pempty);
 
 	}
@@ -290,21 +343,26 @@ int crawl(char *start_url,
 	//hash_init();
 	p_init();
 	d_init();
+//printf("start url = %s\n", start_url);
+	//char *tmp = strtok(start_url," \n"); 
+
 
 	dfill(start_url);
-
+	check_hash(start_url);
 	pthread_t pid[download_workers], cid[parse_workers];
 	int i;
 	
-	for(i = 0; i < download_workers; i++)
+	for(i = 0; i < download_workers; i++){
+		num_awake++;
 		pthread_create(&pid[i], NULL, download_thread, NULL);
-
-	for(i = 0; i < parse_workers; i++)
+	}
+	for(i = 0; i < parse_workers; i++){
+		num_awake++;
 		pthread_create(&cid[i], NULL, parse_thread, NULL);
-	
+	}
 		
-	while(1)
-  		;
+	while(num_awake > 0)//TODO
+  		sleep(1);
 
 
   	return 0;
